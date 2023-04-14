acc0488c744c87199da1010e37f51e034c39d3b1
Murphy <murphyc4@lancaster.ac.uk>
2023-04-14 15:06:08 +0100

    Update files

--- Point processes/Point_process_sims.R
+++ Point processes/Point_process_sims.R
@@ -1,8 +1,37 @@
-Opt  <- readRDS("Point processes/Covariates/Output/data/Opt_S4.RDS")
+library(tidyverse)
+library(dplyr)
 
+Opt  <- readRDS("Point processes/Covariates/Output/data/Opt_S4.RDS")
+Opt
 B0 <- Opt$par[1]
 B1 <- Opt$par[2]
 sigma <- Opt$par[3]
 
+#Covariates
+ics <- readRDS("Point processes/Covariates/00_data/derived/covariates/incremental_Coulomb_stress_mats.RDS")
 
-intensity <- B0*
+#Events
+cat <- read.csv("Data/Events/2022-04-12_15-09-25_cat.csv")
+
+#Groningen outline
+gron_outline <- read.csv("Data/Geophones/Geophone data/Groningen_Field_outline.csv")
+
+#Geophones
+geo <- read.csv("Data/Geophones/Geophone data/Geo_orig_post_process.csv")
+
+plot(cat$RD_X, cat$RD_Y)
+points(gron_outline$X, gron_outline$Y, pch=19, col="blue")
+points(geo$Xcoord, geo$Ycoord, pch=4, col="red", cex=1.5)
+
+#Regular grid
+a <- seq(227750, 269750, by=500)
+b <- seq(565250, 615750, by=500)
+grid <- expand.grid(a,b)
+points(grid$Var1, grid$Var2, pch=19)
+
+icsgrid <- expand.grid(ics$xgrid, ics$ygrid)
+points(icsgrid$Var1, icsgrid$Var2, col="red")
+
+ics$ics[10,10,10]
+
+
