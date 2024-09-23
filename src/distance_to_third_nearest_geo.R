4a40f5a59731052c779129325d265b449c06f3f9
Murphy <murphyc4@lancaster.ac.uk>
2024-09-23 14:02:23 +0100

    Add function to output geophone included in V

--- src/distance_to_third_nearest_geo.R
+++ src/distance_to_third_nearest_geo.R
@@ -18,7 +18,7 @@ euclid_dist_3d(locs, coords)
 geo_current <- function(geophones, date){
   indices_current <- which(geophones$Start_date <= date & geophones$End_date >= date)
   current_geophones <- geophones[indices_current,]
-  current_geo_coords <- data.frame(X=current_geophones$Xcoord, Y=current_geophones$Ycoord, Z=current_geophones$Depth)
+  current_geo_coords <- data.frame(X=current_geophones$Xcoord, Y=current_geophones$Ycoord, Z=current_geophones$Depth, Start_date=current_geophones$Start_date, End_date=current_geophones$End_date)
   return(current_geo_coords)
 }
 
@@ -101,3 +101,38 @@ loc_third_distance_3d <- function(loc, geophones, dates){
   }
   return(third_distances)
 }
+
+What_geos <- function(Eq.df, GeoCoord){
+  Geo_final <- data.frame(X   = numeric(length(Eq.df$X)), Y = numeric(length(Eq.df$X)), Start_date= character(length(Eq.df$X)), End_date= character(length(Eq.df$X)))
+  for(i in 1:length(Eq.df$X)){
+    Eq_current <- Eq.df[i,]
+    Current_coords <- Geo_current(GeoCoord,Eq_current$Date)
+    Eq_loc_current <- data.frame(X=Eq_current$X, Y=Eq_current$Y)
+    Geo_loc_current <- data.frame(X=Current_coords$Xcoord, Y=Current_coords$Ycoord)
+    dist <- sq_eu_dist(Eq_loc_current,Geo_loc_current)
+    df <- data.frame(D=dist,CX=Current_coords$Xcoord, CY=Current_coords$Ycoord, Start_date = Current_coords$Start_date, End_date = Current_coords$End_date)
+    df <- df[order(df$D),]
+    Geo_final[i,1] <- df$CX[3]
+    Geo_final[i,2] <- df$CY[3]
+    Geo_final[i,3] <- df$Start_date[3]
+    Geo_final[i,4] <- df$End_date[3]
+  }
+  return(Geo_final)
+}
+
+what_geos <- function(eq_cat, geophones){
+  included_geos <- data.frame(X=numeric(nrow(eq_cat)), Y=numeric(nrow(eq_cat)), Z=numeric(nrow(eq_cat)), Start_date=character(nrow(eq_cat)), End_date=character(nrow(eq_cat)))
+  print(dim(included_geos))
+  for(i in 1:nrow(eq_cat)){
+    print(i)
+    eq_cat_current <- eq_cat[i,]
+    current_geo_coords <- geo_current(geophones,eq_cat_current$Date)
+    eq_loc_current <- data.frame(X=eq_cat_current$Easting, Y=eq_cat_current$Northing, Z=eq_cat_current$Depth)
+    distances <- euclid_dist_3d(eq_loc_current, current_geo_coords)
+    current_geo_ordered <- current_geo_coords[order(distances),]
+    print(dim(current_geo_ordered[3,]))
+    included_geos[i,] <- current_geo_ordered[3,]
+  }
+  return(included_geos)
+}
+
