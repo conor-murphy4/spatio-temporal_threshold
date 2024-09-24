9204ce4ca7b1f2d195354025a6da5e18d79740dd
Murphy <murphyc4@lancaster.ac.uk>
2024-09-24 11:43:16 +0100

    Tidied and added code to visualise thresholds

--- threshold_results_split_catalogue.R
+++ threshold_results_split_catalogue.R
@@ -65,29 +65,35 @@ pc_thresh_after <- piecewise_const_thresh[(u_h_length+1):length(mags)]
 pc_belowV <- piecewise_const_thresh[gron_eq_cat$V_3d <= 5]
 pc_aboveV <- piecewise_const_thresh[gron_eq_cat$V_3d > 5]
 
-# V-based thresholds
+# V-based thresholds ------------------------------------------------------
+geo_thresh_fit_before <- readRDS("threshold_results/geo_thresh_fit_before.rds")
 geo_thresh_fit_3d_before <- readRDS("threshold_results/geo_thresh_fit_3d_before.rds")
 log_geo_thresh_fit_3d_before <- readRDS("threshold_results/log_geo_thresh_fit_3d_before.rds")
 sqrt_geo_thresh_fit_3d_before <- readRDS("threshold_results/sqrt_geo_thresh_fit_3d_before.rds")
 
+geo_thresh_fit_after <- readRDS("threshold_results/geo_thresh_fit_after.rds")
 geo_thresh_fit_3d_after <- readRDS("threshold_results/geo_thresh_fit_3d_after.rds")
 log_geo_thresh_fit_3d_after <- readRDS("threshold_results/log_geo_thresh_fit_3d_after.rds")
 sqrt_geo_thresh_fit_3d_after <- readRDS("threshold_results/sqrt_geo_thresh_fit_3d_after.rds")
 
+geo_thresh_fit_belowV <- readRDS("threshold_results/geo_thresh_fit_belowV.rds")
 geo_thresh_fit_3d_belowV <- readRDS("threshold_results/geo_thresh_fit_3d_belowV.rds")
 log_geo_thresh_fit_3d_belowV <- readRDS("threshold_results/log_geo_thresh_fit_3d_belowV.rds")
 sqrt_geo_thresh_fit_3d_belowV <- readRDS("threshold_results/sqrt_geo_thresh_fit_3d_belowV.rds")
 
+geo_thresh_fit_aboveV <- readRDS("threshold_results/geo_thresh_fit_aboveV.rds")
 geo_thresh_fit_3d_aboveV <- readRDS("threshold_results/geo_thresh_fit_3d_aboveV.rds")
 log_geo_thresh_fit_3d_aboveV <- readRDS("threshold_results/log_geo_thresh_fit_3d_aboveV.rds")
 sqrt_geo_thresh_fit_3d_aboveV <- readRDS("threshold_results/sqrt_geo_thresh_fit_3d_aboveV.rds")
 
 #Confidence intervals for parameters
 
+#Before and after 06-01-2015
 get_par_ests_step(mags_before, conserv_thresh_before)
 get_par_ests_step(mags_before, eqd_thresh_before)
 get_par_ests_step(mags_before, pc_thresh_before)
 
+get_par_ests_geo(mags_before, geo_thresh_fit_before, third_nearest_dist_before)
 get_par_ests_geo(mags_before, geo_thresh_fit_3d_before, third_nearest_dist_3d_before)
 get_par_ests_geo(mags_before, log_geo_thresh_fit_3d_before, log_third_nearest_dist_3d_before)
 get_par_ests_geo(mags_before, sqrt_geo_thresh_fit_3d_before, sqrt_third_nearest_dist_3d_before)
@@ -96,37 +102,93 @@ get_par_ests_step(mags_after, conserv_thresh_after)
 get_par_ests_step(mags_after, eqd_thresh_after)
 get_par_ests_step(mags_after, pc_thresh_after)
 
+get_par_ests_geo(mags_after, geo_thresh_fit_after, third_nearest_dist_after)
 get_par_ests_geo(mags_after, geo_thresh_fit_3d_after, third_nearest_dist_3d_after)
 get_par_ests_geo(mags_after, log_geo_thresh_fit_3d_after, log_third_nearest_dist_3d_after)
 get_par_ests_geo(mags_after, sqrt_geo_thresh_fit_3d_after, sqrt_third_nearest_dist_3d_after)
+
+#Below and above V=5
+get_par_ests_step(mags_belowV, conserv_belowV)
+get_par_ests_step(mags_belowV, eqd_belowV)
+get_par_ests_step(mags_belowV, pc_belowV)
+
+get_par_ests_geo(mags_belowV, geo_thresh_fit_belowV, third_nearest_dist_belowV)
+get_par_ests_geo(mags_belowV, geo_thresh_fit_3d_belowV, third_nearest_dist_3d_belowV)
+get_par_ests_geo(mags_belowV, log_geo_thresh_fit_3d_belowV, log_third_nearest_dist_3d_belowV)
+get_par_ests_geo(mags_belowV, sqrt_geo_thresh_fit_3d_belowV, sqrt_third_nearest_dist_3d_belowV)
+
+get_par_ests_step(mags_aboveV, conserv_aboveV)
+get_par_ests_step(mags_aboveV, eqd_aboveV)
+get_par_ests_step(mags_aboveV, pc_aboveV)
+
+get_par_ests_geo(mags_aboveV, geo_thresh_fit_aboveV, third_nearest_dist_aboveV)
+get_par_ests_geo(mags_aboveV, geo_thresh_fit_3d_aboveV, third_nearest_dist_3d_aboveV)
+get_par_ests_geo(mags_aboveV, log_geo_thresh_fit_3d_aboveV, log_third_nearest_dist_3d_aboveV)
+get_par_ests_geo(mags_aboveV, sqrt_geo_thresh_fit_3d_aboveV, sqrt_third_nearest_dist_3d_aboveV)
+
 
 # QQ plots
 
 get_qq_plot(mags_before, geo_thresh_fit_3d_before, third_nearest_dist_3d_before)
 
-plot_threshold <- function(gron_eq_cat, thresh_fit, third_nearest_dist){
+# Visualising thresholds
+plot_threshold <- function(gron_eq_cat, thresh_fit, third_nearest_dist, xlab=V){
   threshold <- thresh_fit$thresh[1] + thresh_fit$thresh[2]*third_nearest_dist
-  plot(third_nearest_dist, gron_eq_cat$Magnitude, xlab = "Third nearest distance", ylab = "Magnitude")
+  plot(third_nearest_dist, gron_eq_cat$Magnitude, ylab = "Magnitude", xlab=xlab)
   points(third_nearest_dist, threshold, col="red", pch=19)
 
 }
 
-plot_threshold(gron_eq_cat_before, geo_thresh_fit_3d_before, third_nearest_dist_3d_before)
+# Against V, logV and sqrt(V)
+dev.new(height=12, width=30, noRStudioGD = TRUE)
+par(mfrow=c(1,3), bg="transparent")
+plot_threshold(gron_eq_cat, geo_thresh_fit_3d, third_nearest_dist_3d, xlab = "V_3d")
+points(third_nearest_dist_3d_before, geo_thresh_fit_3d_before$thresh[1] + geo_thresh_fit_3d_before$thresh[2]*third_nearest_dist_3d_before, col="blue", pch=19)
+points(third_nearest_dist_3d_after, geo_thresh_fit_3d_after$thresh[1] + geo_thresh_fit_3d_after$thresh[2]*third_nearest_dist_3d_after, col="green", pch=19)
+points(third_nearest_dist_3d_belowV, geo_thresh_fit_3d_belowV$thresh[1] + geo_thresh_fit_3d_belowV$thresh[2]*third_nearest_dist_3d_belowV, col="purple", pch=19)
+points(third_nearest_dist_3d_aboveV, geo_thresh_fit_3d_aboveV$thresh[1] + geo_thresh_fit_3d_aboveV$thresh[2]*third_nearest_dist_3d_aboveV, col="orange", pch=19)
+legend("bottomright", legend=c("All", "Before", "After", "Below V=5", "Above V=5"), col=c("red", "blue", "green", "purple", "orange"), pch=19)
 
+plot_threshold(gron_eq_cat, log_geo_thresh_fit_3d, log_third_nearest_dist_3d, xlab = "log(V_3d)")
+points(log_third_nearest_dist_3d_before, log_geo_thresh_fit_3d_before$thresh[1] + log_geo_thresh_fit_3d_before$thresh[2]*log_third_nearest_dist_3d_before, col="blue", pch=19)
+points(log_third_nearest_dist_3d_after, log_geo_thresh_fit_3d_after$thresh[1] + log_geo_thresh_fit_3d_after$thresh[2]*log_third_nearest_dist_3d_after, col="green", pch=19)
+points(log_third_nearest_dist_3d_belowV, log_geo_thresh_fit_3d_belowV$thresh[1] + log_geo_thresh_fit_3d_belowV$thresh[2]*log_third_nearest_dist_3d_belowV, col="purple", pch=19)
+points(log_third_nearest_dist_3d_aboveV, log_geo_thresh_fit_3d_aboveV$thresh[1] + log_geo_thresh_fit_3d_aboveV$thresh[2]*log_third_nearest_dist_3d_aboveV, col="orange", pch=19)
+legend("bottomright", legend=c("All", "Before", "After", "Below V=5", "Above V=5"), col=c("red", "blue", "green", "purple", "orange"), pch=19)
 
+plot_threshold(gron_eq_cat, sqrt_geo_thresh_fit_3d, sqrt_third_nearest_dist_3d, xlab = "sqrt(V_3d)")
+points(sqrt_third_nearest_dist_3d_before, sqrt_geo_thresh_fit_3d_before$thresh[1] + sqrt_geo_thresh_fit_3d_before$thresh[2]*sqrt_third_nearest_dist_3d_before, col="blue", pch=19)
+points(sqrt_third_nearest_dist_3d_after, sqrt_geo_thresh_fit_3d_after$thresh[1] + sqrt_geo_thresh_fit_3d_after$thresh[2]*sqrt_third_nearest_dist_3d_after, col="green", pch=19)
+points(sqrt_third_nearest_dist_3d_belowV, sqrt_geo_thresh_fit_3d_belowV$thresh[1] + sqrt_geo_thresh_fit_3d_belowV$thresh[2]*sqrt_third_nearest_dist_3d_belowV, col="purple", pch=19)
+points(sqrt_third_nearest_dist_3d_aboveV, sqrt_geo_thresh_fit_3d_aboveV$thresh[1] + sqrt_geo_thresh_fit_3d_aboveV$thresh[2]*sqrt_third_nearest_dist_3d_aboveV, col="orange", pch=19)
+legend("bottomright", legend=c("All", "Before", "After", "Below V=5", "Above V=5"), col=c("red", "blue", "green", "purple", "orange"), pch=19)
 
-# After changepoint
+#Against index
+dev.new(height=12, width=30, noRStudioGD = TRUE)
+par(mfrow=c(1,3), bg="transparent")
 
-get_par_ests(mags_after, geo_thresh_fit_3d_after, third_nearest_dist_3d_after)
-get_par_ests(mags_after, log_geo_thresh_fit_3d_after, log_third_nearest_dist_3d_after)
-get_par_ests(mags_after, sqrt_geo_thresh_fit_3d_after, sqrt_third_nearest_dist_3d_after)
+plot(gron_eq_cat$Magnitude, ylab = "Magnitude", xlab="Index", main="V_3d")
+points(geo_thresh_fit_3d$thresh[1] + geo_thresh_fit_3d$thresh[2]*third_nearest_dist_3d, col="red", pch=19)
+points(geo_thresh_fit_3d_before$thresh[1] + geo_thresh_fit_3d_before$thresh[2]*third_nearest_dist_3d_before, col="blue", pch=19)
+points(geo_thresh_fit_3d_after$thresh[1] + geo_thresh_fit_3d_after$thresh[2]*third_nearest_dist_3d_after, col="green", pch=19)
+points(geo_thresh_fit_3d_belowV$thresh[1] + geo_thresh_fit_3d_belowV$thresh[2]*third_nearest_dist_3d_belowV, col="purple", pch=19)
+points(geo_thresh_fit_3d_aboveV$thresh[1] + geo_thresh_fit_3d_aboveV$thresh[2]*third_nearest_dist_3d_aboveV, col="orange", pch=19)
+legend("bottomright", legend=c("All", "Before", "After", "Below V=5", "Above V=5"), col=c("red", "blue", "green", "purple", "orange"), pch=19)
 
+plot(gron_eq_cat$Magnitude, ylab = "Magnitude", xlab="Index", main="log(V_3d)")
+points(log_geo_thresh_fit_3d$thresh[1] + log_geo_thresh_fit_3d$thresh[2]*log_third_nearest_dist_3d, col="red", pch=19)
+points(log_geo_thresh_fit_3d_before$thresh[1] + log_geo_thresh_fit_3d_before$thresh[2]*log_third_nearest_dist_3d_before, col="blue", pch=19)
+points(log_geo_thresh_fit_3d_after$thresh[1] + log_geo_thresh_fit_3d_after$thresh[2]*log_third_nearest_dist_3d_after, col="green", pch=19)
+points(log_geo_thresh_fit_3d_belowV$thresh[1] + log_geo_thresh_fit_3d_belowV$thresh[2]*log_third_nearest_dist_3d_belowV, col="purple", pch=19)
+points(log_geo_thresh_fit_3d_aboveV$thresh[1] + log_geo_thresh_fit_3d_aboveV$thresh[2]*log_third_nearest_dist_3d_aboveV, col="orange", pch=19)
+legend("bottomright", legend=c("All", "Before", "After", "Below V=5", "Above V=5"), col=c("red", "blue", "green", "purple", "orange"), pch=19)
 
-get_par_ests_geo(mags_belowV, geo_thresh_fit_3d_belowV, third_nearest_dist_3d_belowV)
-get_par_ests_geo(mags_belowV, log_geo_thresh_fit_3d_belowV, log_third_nearest_dist_3d_belowV)
-get_par_ests_geo(mags_belowV, sqrt_geo_thresh_fit_3d_belowV, sqrt_third_nearest_dist_3d_belowV)
+plot(gron_eq_cat$Magnitude, ylab = "Magnitude", xlab="Index", main="sqrt(V_3d)")
+points(sqrt_geo_thresh_fit_3d$thresh[1] + sqrt_geo_thresh_fit_3d$thresh[2]*sqrt_third_nearest_dist_3d, col="red", pch=19)
+points(sqrt_geo_thresh_fit_3d_before$thresh[1] + sqrt_geo_thresh_fit_3d_before$thresh[2]*sqrt_third_nearest_dist_3d_before, col="blue", pch=19)
+points(sqrt_geo_thresh_fit_3d_after$thresh[1] + sqrt_geo_thresh_fit_3d_after$thresh[2]*sqrt_third_nearest_dist_3d_after, col="green", pch=19)
+points(sqrt_geo_thresh_fit_3d_belowV$thresh[1] + sqrt_geo_thresh_fit_3d_belowV$thresh[2]*sqrt_third_nearest_dist_3d_belowV, col="purple", pch=19)
+points(sqrt_geo_thresh_fit_3d_aboveV$thresh[1] + sqrt_geo_thresh_fit_3d_aboveV$thresh[2]*sqrt_third_nearest_dist_3d_aboveV, col="orange", pch=19)
+legend("bottomright", legend=c("All", "Before", "After", "Below V=5", "Above V=5"), col=c("red", "blue", "green", "purple", "orange"), pch=19)
 
-get_par_ests_geo(mags_aboveV, geo_thresh_fit_3d_aboveV, third_nearest_dist_3d_aboveV)
-get_par_ests_geo(mags_aboveV, log_geo_thresh_fit_3d_aboveV, log_third_nearest_dist_3d_aboveV)
-get_par_ests_geo(mags_aboveV, sqrt_geo_thresh_fit_3d_aboveV, sqrt_third_nearest_dist_3d_aboveV)
 
