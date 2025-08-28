844c3e2286b4b040ddc004038bce0530593fbebc
Zak D Varty <44709288+zakvarty@users.noreply.github.io>
2025-08-28 11:23:11 +0100

    lint intensity estimation functions

--- src/intensity_estimation.R
+++ src/intensity_estimation.R
@@ -1,6 +1,10 @@
 
 # Function for estimation of intensity above 0 given geophone based threshold
 
+grid_box <- function(direction){
+  (max(unique(direction)) - min(unique(direction))) / length(unique(direction))
+}
+
 Poisson_process_LL_icsmax <- function(par, data, covariates, threshold_obs, covariates_threshold, thresh_fit){
   # par: parameter vector
   # data: gron_eq_cat including observed locations/mags/covariates
@@ -17,12 +21,12 @@ Poisson_process_LL_icsmax <- function(par, data, covariates, threshold_obs, cova
   exceedances <- data[data$Magnitude > threshold_obs, ]
   threshold_ex <- threshold_obs[data$Magnitude > threshold_obs]
   
-  # Removing locations where dsmaxdt= 0
+  # Removing locations where dsmaxdt = 0
   exceedances_ds <- exceedances[exceedances$dsmaxdt > 0, ]
   threshold_ex_ds <- threshold_ex[exceedances$dsmaxdt > 0]
   
   # Compute intensity on observed exceedances
-  intensity_0_obs <- exceedances_ds$dsmaxdt*exp(gamma_0 + gamma_1 * exceedances_ds$ICS_max)
+  intensity_0_obs <- exceedances_ds$dsmaxdt * exp(gamma_0 + gamma_1 * exceedances_ds$ICS_max)
   sigma_var_0_obs <- thresh_fit$par[1] + thresh_fit$par[2] * exceedances_ds$ICS_max
   intensity_above_threshold_obs <- intensity_0_obs * (1 + thresh_fit$par[3] * (threshold_ex_ds) / sigma_var_0_obs)^(-1 / thresh_fit$par[3])
   
@@ -30,23 +34,20 @@ Poisson_process_LL_icsmax <- function(par, data, covariates, threshold_obs, cova
   LL_1 <- sum(log(intensity_above_threshold_obs))
   
   # Compute integrated intensity across space and time
-  intensity_0 <- covariates$dsmaxdt*exp(gamma_0 + gamma_1 * covariates$ICS_max)
+  intensity_0 <- covariates$dsmaxdt * exp(gamma_0 + gamma_1 * covariates$ICS_max)
   sigma_var_0 <- thresh_fit$par[1] + thresh_fit$par[2] * covariates$ICS_max
   intensity_above_threshold <- intensity_0 * (1 + thresh_fit$par[3] * (covariates_threshold) / sigma_var_0)^(-1 / thresh_fit$par[3])
   intensity_above_threshold[1 + thresh_fit$par[3] * (covariates_threshold) / sigma_var_0 < 0] <- 0
-  grid_box_E <- (max(unique(covariates$Easting))-min(unique(covariates$Easting)))/length(unique(covariates$Easting))
-  grid_box_N <- (max(unique(covariates$Northing))-min(unique(covariates$Northing)))/length(unique(covariates$Northing))
-  grid_box_area <- grid_box_E/1000* grid_box_N/1000
-  integrated_intensity <- sum(intensity_above_threshold, na.rm=T)*grid_box_area
-  
+
+  grid_box_E <- grid_box(covariates$Easting)
+  grid_box_N <- grid_box(covariates$Northing)
+  grid_box_area <- grid_box_E / 1000 * grid_box_N / 1000
+  integrated_intensity <- sum(intensity_above_threshold, na.rm = TRUE) * grid_box_area
   
   # Compute log-likelihood term 2
   LL_2 <- -integrated_intensity
   
-  # Compute log-likelihood
-  LL <- LL_1 + LL_2
-  
-  return(LL)
+  return(LL_1 + LL_2)
 }
 
 # Function for intensity estimation above the conservative or other constant threshold
@@ -63,38 +64,39 @@ Poisson_process_LL_const_thresh <- function(par, data, covariates, threshold=1.4
   gamma_0 <- par[1]
   gamma_1 <- par[2]
   
+  # earthquakes exceeding threshild magnitude at locations where dsmaxdt > 0
   exceedances <- data[data$Magnitude > threshold, ]
-  exceedances_ds <- exceedances[exceedances$dsmaxdt > 0, ]  # Removing locations where dsmaxdt = 0
+  exceedances_ds <- exceedances[exceedances$dsmaxdt > 0, ]  
   
   # Compute intensity on observed exceedances
-  intensity_thresh_obs <- exceedances_ds$dsmaxdt*exp(gamma_0 + gamma_1 * exceedances_ds$ICS_max)
+  intensity_thresh_obs <- exceedances_ds$dsmaxdt * exp(gamma_0 + gamma_1 * exceedances_ds$ICS_max)
   
   # Compute log-likelihood term 1
   LL_1 <- sum(log(intensity_thresh_obs))
   
   # Compute integrated intensity across space and time
-  intensity_thresh <- covariates$dsmaxdt*exp(gamma_0 + gamma_1 * covariates$ICS_max)
+  intensity_thresh <- covariates$dsmaxdt * exp(gamma_0 + gamma_1 * covariates$ICS_max)
   
-  grid_box_E <- (max(unique(covariates$Easting))-min(unique(covariates$Easting)))/length(unique(covariates$Easting))
-  grid_box_N <- (max(unique(covariates$Northing))-min(unique(covariates$Northing)))/length(unique(covariates$Northing))
-  grid_box_area <- grid_box_E/1000* grid_box_N/1000
+  grid_box_E <- grid_box(covariates$Easting)
+  grid_box_N <- grid_box(covariates$Northing)
+  grid_box_area <- grid_box_E / 1000 * grid_box_N / 1000
   
-  integrated_intensity <- sum(intensity_thresh)*grid_box_area
-  
+  integrated_intensity <- sum(intensity_thresh) * grid_box_area
   
   # Compute log-likelihood term 2
   LL_2 <- -integrated_intensity
   
-  # Compute log-likelihood
-  LL <- LL_1 + LL_2
-  
-  return(LL)
+  return(LL_1 + LL_2)
 }
 
 resulting_intensity_icsmax <- function(opt_PP, covariates, covariates_threshold, thresh_fit){
-  intensity_0 <- covariates$dsmaxdt*exp(opt_PP$par[1] + opt_PP$par[2] * covariates$ICS_max)
+  
+  intensity_0 <- covariates$dsmaxdt * exp(opt_PP$par[1] + opt_PP$par[2] * covariates$ICS_max)
+  
   sigma_var_0 <- thresh_fit$par[1] + thresh_fit$par[2] * covariates$ICS_max
+  
   intensity_above_threshold <- intensity_0 * (1 + thresh_fit$par[3] * (covariates_threshold) / sigma_var_0)^(-1 / thresh_fit$par[3])
+  
   intensity_above_threshold[1 + thresh_fit$par[3] * (covariates_threshold) / sigma_var_0 < 0] <- 0
   return(intensity_above_threshold)
 }
